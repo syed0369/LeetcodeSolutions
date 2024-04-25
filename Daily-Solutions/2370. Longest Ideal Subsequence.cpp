088371246653281b4fa78469fff649206e0459b1
syed0369 <126373476+syed0369@users.noreply.github.com>
2024-04-25 14:33:06 +0530

    Update 2370. Longest Ideal Subsequence.cpp

--- Daily-Solutions/2370. Longest Ideal Subsequence.cpp
+++ Daily-Solutions/2370. Longest Ideal Subsequence.cpp
@@ -10,6 +10,12 @@ Return the length of the longest ideal string.
 Solution:
 For each character in s (say s[i]), find the longest ideal string by including it in the longest ideal string whose last character is in the range s[i] - k and s[i] + k. Maintain the longest lengths for each character in an array. Return the maximum value in this array.
 
+
+Time Complexity:   O(N)
+Space Complexity:  O(1)
+
+where N is length of string.
+
 ********************************************/
 class Solution {
 public:
@@ -17,13 +23,15 @@ public:
         int i, j;
         int l, r;
         int maxlength;
-        vector<int> alpha(26, 0);
+        vector<int> alpha(26, 0); // for storing the max length of the ideal string ending at letter a to z.
         maxlength = 0;
         for(i = 0; i < s.length(); i++) {
-            l = s[i] - 'a' - k;
-            r = s[i] - 'a' + k;
+            l = s[i] - 'a' - k; // lower bound check of alphabet
+            r = s[i] - 'a' + k; // upper bound check of alphabet
+            // finding max length of ideal string having the last letters in range of 'a' + max(l, 0) to 'a' + min(r, 25)
             for(j = max(l, 0); j <= min(r, 25); j++)
                 alpha[s[i] - 'a'] = max(alpha[s[i] - 'a'], alpha[j]);
+            // Adding the character s[i] to the new ideal string and updating maxlength.
             maxlength = max(maxlength, ++alpha[s[i] - 'a']);
         }
         return maxlength;
