2d059e073ee2aa9a85fd913d41a66380a9441b25
syed0369 <126373476+syed0369@users.noreply.github.com>
2024-05-03 23:34:17 +0530

    Update 2024-05-02-Largest-Positive-Integer-That-Exists-With-Its-Negative.cpp

--- Daily-Solutions/2024-05-02-Largest-Positive-Integer-That-Exists-With-Its-Negative.cpp
+++ Daily-Solutions/2024-05-02-Largest-Positive-Integer-That-Exists-With-Its-Negative.cpp
@@ -1,3 +1,19 @@
+/********************************************
+Solution to LeetCode Daily Question 05-02-2024. 
+Problem:
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+
+Solution:
+Create a hash table with the elements as the keys and assign 1 value if the element is present. For each element also check if its negative is present in the hash table and compare the value with current maximum. This can be done in a single pass.
+
+Time Complexity:   O(N) average time for access incase of unordered_map is in constant time, if map used then O(log n)
+Space Complexity:  O(1) 
+
+where N is length of string.
+
+********************************************/
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
