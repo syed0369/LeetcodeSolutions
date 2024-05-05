cec3cc0cfa2b431a2808142ed34f2f3421eb0773
syed0369 <126373476+syed0369@users.noreply.github.com>
2024-05-05 12:30:57 +0530

    Update 2024-05-05-Delete-Node-in-a-Linked-List.cpp

--- Daily-Solutions/2024-05-05-Delete-Node-in-a-Linked-List.cpp
+++ Daily-Solutions/2024-05-05-Delete-Node-in-a-Linked-List.cpp
@@ -1,3 +1,28 @@
+/********************************************
+Solution to LeetCode Daily Question 05-05-2024. 
+
+Problem:
+There is a singly-linked list head and we want to delete a node node in it.
+
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before node should be in the same order.
+All the values after node should be in the same order.
+
+Solution:
+Since only forward traversal is possible, we cannot delete the node with the address given as the argument to the function. The idea is to copy the value of its successor to the val field of the node. Then the successor the node has to be deleted which can be done by setting the next pointer of the node originally to be deleted to reference the node pointed by its successor.
+
+Complexity:
+Time complexity: O(1) 
+Space complexity: O(1)
+
+********************************************/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
