3241125612859d08fd69dbcf5aa536000728b294
Syed Umair <126373476+syed0369@users.noreply.github.com>
2024-05-10 13:00:30 +0530

    Update 2024-05-09-Maximize-Happiness-of-Selected-Children.cpp

--- Daily-Solutions/2024-05-09-Maximize-Happiness-of-Selected-Children.cpp
+++ Daily-Solutions/2024-05-09-Maximize-Happiness-of-Selected-Children.cpp
@@ -1,3 +1,23 @@
+/********************************************
+Solution to LeetCode Daily Question 05-05-2024. 
+
+Problem:
+You are given an array happiness of length n, and a positive integer k.
+
+There are n children standing in a queue, where the ith child has happiness value happiness[i]. You want to select k children from these n children in k turns.
+
+In each turn, when you select a child, the happiness value of all the children that have not been selected till now decreases by 1. Note that the happiness value cannot become negative and gets decremented only if it is positive.
+
+Return the maximum sum of the happiness values of the selected children you can achieve by selecting k children.
+
+Solution:
+Sine we have to select the maximum happiness in each iteration, priority queue pq can be used. In C++ it is implemented using heap. First inserting the elements of happiness into the pq named q. For selecting the maximum happiness in each iteration we can just pop out the element from the pq. Since for each turn the happiness of the children decreases by 1, maintain a counter which increases after each element is selected.
+
+Complexity:
+Time complexity: O(nlogn + klogn) 
+Space complexity: O(n)
+where n is number of children
+********************************************/
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
