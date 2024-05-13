537d5b3fe427167a6d35f181dee7962e17704c5f
Syed Umair <126373476+syed0369@users.noreply.github.com>
2024-05-13 23:47:42 +0530

    Update 2024-05-13-Score-After-Flipping-Matrix.cpp

--- Daily-Solutions/2024-05-13-Score-After-Flipping-Matrix.cpp
+++ Daily-Solutions/2024-05-13-Score-After-Flipping-Matrix.cpp
@@ -2,21 +2,27 @@
 Solution to LeetCode Daily Question 05-13-2024. 
 
 Problem:
-You are given an array happiness of length n, and a positive integer k.
+You are given an m x n binary matrix grid.
 
-There are n children standing in a queue, where the ith child has happiness value happiness[i]. You want to select k children from these n children in k turns.
+A move consists of choosing any row or column and toggling each value in that row or column (i.e., changing all 0's to 1's, and all 1's to 0's).
 
-In each turn, when you select a child, the happiness value of all the children that have not been selected till now decreases by 1. Note that the happiness value cannot become negative and gets decremented only if it is positive.
+Every row of the matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
 
-Return the maximum sum of the happiness values of the selected children you can achieve by selecting k children.
+Return the highest possible score after making any number of moves (including zero moves).
 
 Solution:
-Sine we have to select the maximum happiness in each iteration, priority queue pq can be used. In C++ it is implemented using heap. First inserting the elements of happiness into the pq named q. For selecting the maximum happiness in each iteration we can just pop out the element from the pq. Since for each turn the happiness of the children decreases by 1, maintain a counter which increases after each element is selected.
+The most significant bit has to be set to obtain the maximum sum. If the bit is 1 we leave the row unchanged if it is 0, we flip the bits in the row. Now we look at the columns, if the number of ones is less than the number of zeroes, we should also keep account of whether particular row in which the bit is present was flipped or not. This can be checked form the table:
+First Element | Current Element	|Current Element after flip
+     0	      |        0	    |       1
+     0	      |        1	    |       0
+     1        |        0	    |       0
+     1	      |        1	    |       1
+If the first row element is 0, flip the curr element else donot flip. 
+We can count the number of ones in the particular column after which we can choose to flip the bits if count of ones is less than the count of zeroes. The decimal form of the number is then adder to score. The decimal form is  1  << n - j - 1. Where i,j is the index of the bit in the matrix and n is the number of columns. 
 
 Complexity:
-Time complexity: O(nlogn + klogn) Any operation takes logn time. For creating the pq, n insertions required O(nlongn) for accessing the maximum element it takes O(1), deleting it takes longn time, k deletions take O(klogn). Hence total time complexity O(nlogn + klogn)
-Space complexity: O(n)
-where n is number of children
+Time complexity: O(m * n)
+Space complexity: O(1)
 ********************************************/
 
 class Solution {
