4eab60d89ac973ef5c2c85c9ba2a02fc1ddd5007
Syed Umair <126373476+syed0369@users.noreply.github.com>
2024-05-16 16:13:32 +0530

    Update 2024-05-16-Evaluate-Boolean-Binary-Tree.cpp

--- Daily-Solutions/2024-05-16-Evaluate-Boolean-Binary-Tree.cpp
+++ Daily-Solutions/2024-05-16-Evaluate-Boolean-Binary-Tree.cpp
@@ -1,3 +1,27 @@
+/********************************************
+Solution to LeetCode Daily Question 05-13-2024. 
+
+Problem:
+You are given the root of a full binary tree with the following properties:
+
+Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+
+The evaluation of a node is as follows:
+
+If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+Return the boolean result of evaluating the root node.
+
+Solution:
+Using DFS, each internal node expression an be evaluated. The value of expression at a particular node will be the result of the expression in the left and right subtrees ORed or ADDed depending on the value in the node. For leaf nodes, the value is the val field of the node itself.
+The evaluation for the left and the right subtree of the current node is done recursively. When a leaf node is encountered, the val is returned.
+Complexity:
+Time complexity: O(n)
+Space complexity: O(n)
+n is number of nodes in the tree
+********************************************/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
