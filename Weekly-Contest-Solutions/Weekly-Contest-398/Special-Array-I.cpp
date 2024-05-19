c52c06859245d589d734d2ff8921c87b1b5488d4
Syed Umair <126373476+syed0369@users.noreply.github.com>
2024-05-19 21:34:33 +0530

    Update Special-Array-I.cpp

--- Weekly-Contest-Solutions/Weekly-Contest-398/Special-Array-I.cpp
+++ Weekly-Contest-Solutions/Weekly-Contest-398/Special-Array-I.cpp
@@ -1,3 +1,17 @@
+/********************************************
+Problem:
+An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
+Solution:
+Iterate through the array and check the parity adjacent elements.
+
+Time Complexity:   O(N)
+Space Complexity:  O(1)
+
+where N is size of the vector.
+
+********************************************/
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
